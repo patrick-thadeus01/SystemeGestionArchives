0ca62902232588cda6db9e6a10e0fddf514e5442
Patrick Thadeus <patrickichu058@gmail.com>
2025-09-28 01:49:56 +0100

    login page

--- admin/login_query.php
+++ admin/login_query.php
@@ -1,15 +1,19 @@
 <?php
 	session_start();
 	require 'conn.php';
-	
+	//if the user got registered
 	if(ISSET($_POST['login'])){
 		$username = $_POST['username'];
 		$password = md5($_POST['password']);
 		
+		//query to check if the username and password exist in the database
+		//if it exists, it will return one row
 		$query = mysqli_query($conn, "SELECT * FROM `user` WHERE `username` = '$username' && `password` = '$password'") or die(mysqli_error());
 		$fetch = mysqli_fetch_array($query);
 		$row = $query->num_rows;
 		
+		//if one row is returned, it means the username and password is correct
+		//then the program creates a session and redirect to home.php
 		if($row > 0){
 			$_SESSION['user'] = $fetch['user_id'];
 			$_SESSION['status'] = $fetch['status'];
